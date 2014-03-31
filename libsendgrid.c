7c587b0728a3c3bc61ec2ccd27d08377e6a1d6fb
rbin <me@rbin.co>
2014-03-31 22:02:12 +0100

    Didn't mean to make you cry,

--- libsendgrid.c
+++ libsendgrid.c
@@ -7,23 +7,17 @@ static const char* sgUrl = "https://api.sendgrid.com/api/mail.send.json";
 int 
 send_email(char *sgUsername, char *sgPassword, char *sgToEmail, char *sgFromEmail, char *sgSubject, char *sgBodyText)
 {
+
   CURL *curl;
-  CURLcode res;
- 
-  curl = curl_easy_init();
-  if(curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, sgUrl);
- 
-    /* Perform the request, res will get the return code */ 
-    res = curl_easy_perform(curl);
-    /* Check for errors */ 
-    if(res != CURLE_OK)
-      fprintf(stderr, "curl_easy_perform() failed: %s\n",
-              curl_easy_strerror(res));
- 
-    /* always cleanup */ 
-    curl_easy_cleanup(curl);
-  }
+  curl_global_init();
+  curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
+  curl_easy_setopt(curl, CURLOPT_URL, sgUrl);
+  curl_easy_setopt(curl, CURLOPT_POST, 1);
+  curl_easy_setopt(curl, CURLOPT_USERNAME, sgUsername);
+  curl_easy_setopt(curl, CURLOPT_PASSWORD, sgPassword);
+  curl_easy_setopt(curl, CURLOPT_POSTFIELDS, /* sgToEmail, sgFromEmail, sgSubject, sgBodyText */  );
+  curl_easy_perform(curl);
+  curl_easy_cleanup(curl);
 
   return 0;
 }
