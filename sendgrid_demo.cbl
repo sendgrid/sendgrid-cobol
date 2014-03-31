97d6d9a64bf75693990c4052361c7cf988cbb96b
Alex Reed <alexreed@outlook.com>
2014-03-31 19:32:11 +0100

    Is this just fantasy?

---
+++ sendgrid_demo.cbl
@@ -0,0 +1,14 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.   sendgrid-demo.
+       AUTHOR.       Robin Johnson. Alex Reed.
+
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+
+
+       PROCEDURE DIVISION.
+
+       END PROGRAM
