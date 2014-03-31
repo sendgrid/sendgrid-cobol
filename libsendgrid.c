ccdf36c40367df9869408a14644735755a741361
rbin <me@rbin.co>
2014-03-31 20:33:47 +0100

    I'm just a poor boy, I need no sympathy,

--- libsendgrid.c
+++ libsendgrid.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <curl/curl.h>
 
-int sg_test(char *sgUsername, char *sgPassword, char *sgUrl);
+int 
+send_email(char *sgUsername, char *sgPassword, char *sgUrl, char *sgToEmail, char *sgFromEmail, char *sgSubject, char *sgBodyText)
 
 int
 sg_test(char *sgUsername, char *sgPassword, char *sgUrl)
@@ -14,8 +15,37 @@ sg_test(char *sgUsername, char *sgPassword, char *sgUrl)
   return 0;
 }
 
-function_pt(void *ptr, size_t size, size_t nmemb, void *stream)
+int
+send_email(char *sgUsername, char *sgPassword, char *sgUrl, char *sgToEmail, char *sgFromEmail, char *sgSubject, char *sgBodyText)
+
+
+// LOOK HERE
+
+curl -d 'to=robin@sendgrid.com&toname=Robin Johnson&subject=LOL EMAIL&text=testingtextbody&from=rbin@sendgrid.com&api_user=$SENDGRID_USERNAME&api_key=$SENDGRID_PASSWORD' https://api.sendgrid.com/api/mail.send.json
+
+//
+
+
+int 
+main(void)
 {
-  printf("%d", atoi(ptr));
+  CURL *curl;
+  CURLcode res;
+ 
+  curl = curl_easy_init();
+  if(curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, "https://api.sendgrid.com/v3/user/email");
+ 
+    /* Perform the request, res will get the return code */ 
+    res = curl_easy_perform(curl);
+    /* Check for errors */ 
+    if(res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
+ 
+    /* always cleanup */ 
+    curl_easy_cleanup(curl);
+  }
+  return 0;
 }
 
